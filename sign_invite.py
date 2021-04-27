2ddced27e4be28a0e895455a6aa59da0fd0db73e
lingxi1322 <78313364+lingxi1322@users.noreply.github.com>
2021-04-27 10:42:37 +0800

    Update sign_invite.py

--- sign_invite.py
+++ sign_invite.py
@@ -39,18 +39,17 @@ def push_wechat(txt):
 def main():
     # 被邀请人的 wps_sid 列表，
     sid_list = [
-        "V02StVuaNcoKrZ3BuvJQ1FcFS_xnG2k00af250d4002664c02f",
-        "V02SWIvKWYijG6Rggo4m0xvDKj1m7ew00a8e26d3002508b828",
-        "V02Sr3nJ9IicoHWfeyQLiXgvrRpje6E00a240b890023270f97",
-        "V02SBsNOf4sJZNFo4jOHdgHg7-2Tn1s00a338776000b669579",
-        "V02S2oI49T-Jp0_zJKZ5U38dIUSIl8Q00aa679530026780e96",
-        "V02ShotJqqiWyubCX0VWTlcbgcHqtSQ00a45564e002678124c",
-        "V02SFiqdXRGnH5oAV2FmDDulZyGDL3M00a61660c0026781be1",
-        "V02S7tldy5ltYcikCzJ8PJQDSy_ElEs00a327c3c0026782526",
-        "V02SPoOluAnWda0dTBYTXpdetS97tyI00a16135e002684bb5c",
-        "V02Sb8gxW2inr6IDYrdHK_ywJnayd6s00ab7472b0026849b17",
-        "V02SwV15KQ_8n6brU98_2kLnnFUDUOw00adf3fda0026934a7f",
-        "V02S8fWiw86zsFg1oE5p4hCHAR6GPE000a4e69e2002508b828"
+    "V02Sw9-goMQAxyEQu_EULMetb-INZo000af1142c00479959d8",
+    "V02S0R96lUd8ZEzQtyjxZX-kHQ2_tpk00a28f1650047c345ee",
+    "V02SBxbHOhf4rcYlBaXzTfJ8WC3B6PE00aa5f34d0038de5bac",
+    "V02SRvwTFf68enT4JrOPhQkKrKKqv5k00ab38f8500380673c8",
+    "V02SS1Aj322-C9Z-P36iZF2epL78zOQ00a7ea26400300425d3",
+    "V02S96aSosktKJiRKwEwBhqiWKHhxkE00a01300600374985fe",
+    "V02SFiqdXRGnH5oAV2FmDDulZyGDL3M00a61660c0026781be1",
+    "V02SzpPttrDh9DtVZZjuSixmKOYsAuU00a98ee4a0047756591",
+    "V02SkTszoOYP88gtXvH1cXoDrMGoEGg00af1f571004626b252",
+    "V02SEdbqgnwva8UpfFOiw5BmksbY_JY00a53a2c300461f24f7",
+    "V02S6UQdQhot5IO9pmvBnEuiiVGcCYU00a6bc19c0047b369fa",
 
     ]
     s = requests.session()
@@ -73,7 +72,7 @@ def main():
                 count['fail'].append({'sid':wps_sid[:6]+'***'+wps_sid[-4:], 'msg':'unknown error'})  
             if count["succ"] >= 10 or wps_sid is sid_list[-1]:
                 break
-            time.sleep(2.5) # 间隔2.5秒再邀请
+            time.sleep(10) # 间隔2.5秒再邀请
             
         push_txt = (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).isoformat(sep=' ', timespec='seconds')
         push_txt += "\nWPS签到的邀请推送"
